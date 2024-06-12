4e29f94a1d24e81d6819e4687c46f392f6051678
Meshmanuu <meshanyangu@gmail.com>
2024-06-12 23:51:01 +0300

    class article done

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,46 @@
+# Definition of the class Article
 class Article:
+    all = []  # Class-level list to store all instances of Article
+
     def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
-        
-class Author:
-    def __init__(self, name):
-        self.name = name
+        self.author = author  # Set author using the setter method
+        self.magazine = magazine  # Set magazine using the setter method
+        self._title = None  # Initialize title attribute (private)
+        self.title = title  # Use the setter to validate and set the title
+        Article.all.append(self)  # Add current instance to class-level list 'all'
 
-    def articles(self):
-        pass
+    @property
+    def title(self):
+        return self._title  # Getter for title attribute
 
-    def magazines(self):
-        pass
+    @title.setter
+    def title(self, new_title):
+        if self._title is not None:
+            AttributeError('Title cannot be changed')  # Raise error if title is already set
+        if isinstance(new_title, str) and 5 <= len(new_title) <= 50:
+            self._title = new_title  # Set title if conditions are met
+        else:
+            ValueError('Title must be a string between 5 and 50 characters')  # Raise error for invalid title
 
-    def add_article(self, magazine, title):
-        pass
+    @property
+    def author(self):
+        return self._author  # Getter for author attribute
 
-    def topic_areas(self):
-        pass
+    @author.setter
+    def author(self, new_author):
+        if isinstance(new_author, Author):
+            self._author = new_author  # Set author if it's an instance of Author
+        else:
+            raise TypeError("Author must be an instance of Author")  # Raise error if author is not of type Author
 
-class Magazine:
-    def __init__(self, name, category):
-        self.name = name
-        self.category = category
+    @property
+    def magazine(self):
+        return self._magazine  # Getter for magazine attribute
 
-    def articles(self):
-        pass
+    @magazine.setter
+    def magazine(self, new_magazine):
+        if isinstance(new_magazine, Magazine):
+            self._magazine = new_magazine  # Set magazine if it's an instance of Magazine
+        else:
+            raise TypeError("Magazine must be an instance of Magazine")  # Raise error if magazine is not of type Magazine
 
-    def contributors(self):
-        pass
-
-    def article_titles(self):
-        pass
-
-    def contributing_authors(self):
-        pass
